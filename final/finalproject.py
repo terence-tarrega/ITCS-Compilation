0ae6f87e28a2263d708ad207b2b3b85b68cd439c
terence-tarrega <tterencemartin@gmail.com>
2024-12-06 00:09:12 +0800

    New Updates

--- final/finalproject.py
+++ final/finalproject.py
@@ -1,7 +1,18 @@
 import os
 
 def act():
-    from Activities import ( activity2, activity3, activity4, activity6, activity7, activity8, activity9, activity10, activity11, activity12, activity13, activity14, activity15, activity16, activity17, activity18, activity20)
+    from Activities import ( activity2, activity3, activity4, 
+    activity6, activity7, activity8, 
+    activity9, activity10, activity11, activity12, 
+    activity13, activity14, activity15, activity16, 
+    activity17, activity18, activity20)
+
+
+def code():
+    from Code_challenges import ( code_challenge1, code_challenge2, code_challenge3, code_challenge4, 
+    code_challenge5, code_challenge6, code_challenge6_modified, code_challenge7, code_challenge8, 
+    code_challenge9, code_challenge10, code_challenge11, code_challenge12,
+    code_challenge13, code_challenge14, code_challenge15, code_challenge16 )
 
 
 print("Code Compiler")
@@ -21,9 +32,9 @@ clean()
 #++++++++++++++++++++++++++++++++++++++++++++ACTIVITIES+++++++++++++++++++++++++++++++++++++++++++++++++++
 def activities_selection():
     global act
-    print("++++++++++++++++++++")
-    print("Activities Selection")
-    print("++++++++++++++++++++")
+    print("========================================")
+    print("++++++++++Activities Selection++++++++++")
+    print("========================================")
     print()
     usr = input("Enter the no of which activity you want to access ---> ")
     if usr == "2":
@@ -50,6 +61,59 @@ def activities_selection():
         return(activities_selection())
     
 
+#++++++++++++++++++++++++++++++++++++++++++++++CODE_CHALLENGES++++++++++++++++++++++++++++++++
+
+def code_challenges_selection():
+    print("=============================================")
+    print("++++++++++Code_Challenges Selection++++++++++")
+    print("=============================================")
+    print()
+    usr = input("Enter the no of which Code_Challenges you want to access ---> ")
+    if usr == "1":
+        from Code_challenges import code_challenge1
+    elif usr == "2":
+        from Code_challenges import code_challenge2
+    elif usr == "3":
+        from Code_challenges import code_challenge3
+    elif usr == "4":
+        from Code_challenges import code_challenge4
+    elif usr == "5":
+        from Code_challenges import code_challenge5
+    elif usr == "6":
+        from Code_challenges import code_challenge6
+    elif usr == "7":
+        from Code_challenges import code_challenge7
+    elif usr == "8":
+        from Code_challenges import code_challenge8
+    elif usr == "9":
+        from Code_challenges import code_challenge9
+    elif usr == "10":
+        from Code_challenges import code_challenge10
+    elif usr == "11":
+        from Code_challenges import code_challenge11
+    elif usr == "12":
+        from Code_challenges import code_challenge12
+    elif usr == "13":
+        from Code_challenges import code_challenge13
+    elif usr == "14":
+        from Code_challenges import code_challenge14
+    elif usr == "15":
+        from Code_challenges import code_challenge15
+    elif usr == "16":
+        from Code_challenges import code_challenge16
+    else:
+        print("+++++++++++++++++++++")
+        print("Not a Valid Selection")
+        print("+++++++++++++++++++++")
+        print()
+        input("Press Enter to Continue.....")
+        clean()
+        return(code_challenges_selection())
+
+
+
+
+
 def choices(a,b,c,d):
     pass
     print(f"Welcome {user_name} to Code Compiler 2000!")
@@ -61,16 +125,21 @@ def choices(a,b,c,d):
     print()
     choice = input()
     if choice == "1":
-        print("Activities")
         activities_selection()
+        clean()
     elif choice == "2":
-        print("Code_challenges:")
+        code_challenges_selection()
+        clean()
     elif choice == "3":
         print("Code_challenges2:")
+        clean()
     elif choice == "4":
-        ("yt")
+        print("yt")
+        clean()
     else:
-        print("++++ Invalid Selection, Please Try again ++++")
+        print("=======================================================")
+        print("++++++++++Invalid Selection, Please Try again++++++++++")
+        print("=======================================================")
         print()
         return(choices(a,b,c,d))
 choices(1,2,3,4)
