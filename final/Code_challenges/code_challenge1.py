81d0ea25d3bcc5148adc9e3f5d273f26bd273ccd
terence-tarrega <tterencemartin@gmail.com>
2024-12-10 23:24:14 +0800

    some minor updates

--- final/Code_challenges/code_challenge1.py
+++ final/Code_challenges/code_challenge1.py
@@ -1,4 +1,3 @@
-from final import finalproject
 
-print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\b\b\b\b* \n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\b\b\b\b\b\b* * * \n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\b\b\b\b\b\b\b\b* * * * * \n\t\t\t\t\t\t\t\t\t\t\t\t\t\b\b* * * * * * * \n\t\t\t\t\t\t\t\t\t\t\t\t\t* * * * * \n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\b\b\b\b\b\b* * *  \n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\b\b\b\b*\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
 
+print("\n\t\t\t\t\t\t\t* \n\t\t\t\t\t\t\t\b\b* * * \n\t\t\t\t\t\t\t\b\b\b\b* * * * * \n" + "\t\t\t\t\t\t\t\b\b\b\b\b\b\b\b  * * * * * * * \n\t\t\t\t\t\t\t\b\b\b\b* * * * * \n\t\t\t\t\t\t\t\b\b* * * \n\t\t\t\t\t\t\t*")
